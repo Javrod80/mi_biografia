71a9782fe583fb9debec761224763ef431515e56
Javrod80 <javrod80@gmail.com>
2024-10-16 20:44:48 +0200

    16/10.1

--- mi_biografía/recursos/accion.js
+++ mi_biografía/recursos/accion.js
@@ -4,21 +4,7 @@
 // crear funcionalidad del boton submit del fomulario
 
 
-const botonenviar = document.getElementById("enviar");
-
-botonenviar.addEventListener("click", function(){
-    createHTML("Welcome to the Show");
-    console.log("hola");
-});
-
-
-function createHTML(texto){
-   
-    let etiqueta = document.createElement('p');
-    let contenido = document.createTextNode(texto);
-
-    etiqueta.appendChild(contenido);
-    document.body.appendChild(etiqueta);
-     
-};
-
+function createHTLM(text){
+    text = "hola";
+    console.log(text);
+}
