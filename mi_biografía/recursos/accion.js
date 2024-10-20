e401f4458eda91405f5305f29628cef4da37ae25
Javrod80 <javrod80@gmail.com>
2024-10-20 17:21:04 +0200

    20/10

--- mi_biografía/recursos/accion.js
+++ mi_biografía/recursos/accion.js
@@ -1,10 +1,36 @@
+function muestraMensaje() {
+  const checkbox = document.getElementById("acepto");
 
+  if (!checkbox.checked) {
+    alert("Por favor, acepta los términos para continuar.");
+    return false;
+  }
 
+  alert("Gracias....ya te responderé...");
+  limpiarFormulario();
+}
 
-// crear funcionalidad del boton submit del fomulario
+window.onload = function () {
+  document.getElementById("enviar").onclick = muestraMensaje;
 
+  let inputs = document.getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].type == "text" || inputs[i].type == "email") {
+      inputs[i].onfocus = resaltar;
+      inputs[i].onblur = noresaltar;
+    }
+  }
+};
 
-function createHTLM(text){
-    text = "hola";
-    console.log(text);
+function resaltar() {
+  this.style.backgroundColor = "lightblue";
+  this.style.color = "darkblue";
+}
+
+function noresaltar() {
+  this.style.backgroundColor = "lightgray";
+  this.style.color = "black";
+}
+function limpiarFormulario() {
+  document.getElementById("miFormulario").reset();
 }
