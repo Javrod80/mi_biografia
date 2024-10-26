d2f9d07e8cc37bbe3d4c0767c333cdd06e2da66c
Javrod80 <javrod80@gmail.com>
2024-10-26 20:16:06 +0200

    Actualización

--- mi_biografía/accion.js
+++ mi_biografía/accion.js
@@ -1,19 +1,28 @@
 function muestraMensaje() {
-  const checkbox = document.getElementById("acepto");
+  let checkbox = document.getElementById("acepto");
+  let name = document.getElementById("nombre");
+  let lastName = document.getElementById("apellido");
+  let mail = document.getElementById("email");
+
+  if (!name.value || !lastName.value || !mail.value) {
+    alert("Por favor, rellene todos los campos");
+    return false;
+  }
 
   if (!checkbox.checked) {
     alert("Por favor, acepta los términos para continuar.");
     return false;
   }
-
-  alert("Gracias....ya te responderé...");
+  alert("Hasta la vista Baby!!");
   limpiarFormulario();
 }
 
+// Aqui estoy probando un parte de un código que encontré en los apuntes y me pareció interesante.
 window.onload = function () {
   document.getElementById("enviar").onclick = muestraMensaje;
 
   let inputs = document.getElementsByTagName("input");
+
   for (let i = 0; i < inputs.length; i++) {
     if (inputs[i].type == "text" || inputs[i].type == "email") {
       inputs[i].onfocus = resaltar;
@@ -31,6 +40,7 @@ function noresaltar() {
   this.style.backgroundColor = "lightgray";
   this.style.color = "black";
 }
+
 function limpiarFormulario() {
   document.getElementById("miFormulario").reset();
 }
